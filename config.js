5f5fa418e35079477cc8b225305eba1ed7a83536
Marco Bischof <mbischof@abf.local>
2018-09-21 11:50:42 +0200

    Config update (API-v2)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 Config = {
-	"apiUrl": "https://api-staging.swissunihockey.ch/bo/games/",
+	"apiUrl": "https://api-v2.swissunihockey.ch/bo/games/",
 	"apiParams": "?set=game-report",
 	"outputDirectory": "output",
 	"gameTitleDefault": "Meisterschaft NLA | Runde X | Saison 2017/2018"
